884086a767b771c74ebee0ed182b7ff12bb4be15
newline-rdm <edp@newl.it>
2024-05-13 15:41:48 +0200

    Update conf.py

--- conf.py
+++ conf.py
@@ -25,5 +25,5 @@ language = 'it'
 # https://www.sphinx-doc.org/en/master/usage/configuration.html#options-for-html-output
 
 html_theme = 'sphinx_rtd_theme'
-html_static_path = ['_static']
 html_favicon = '_static/logo.png'
+html_static_path = []
